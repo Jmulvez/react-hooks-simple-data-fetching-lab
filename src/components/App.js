d9b8ed6730ab362b267e9db23aad5c9ed1a02d4c
John <johnmulvey48@yahoo.com>
2022-04-25 10:42:58 -0400

    git@github.com:Jmulvez/react-hooks-simple-data-fetching-lab.git

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,11 @@
-// create your App component here
+import { useEffect } from "react";
+
+function App() {
+    useEffect(() => {
+        fetch('https://dog.ceo/api/breeds/image/random')
+        .then((res) => res.json())
+        .then(data => console.log(data))
+    }, []);
+}
+
+export default App;
